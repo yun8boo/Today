5e390f62e3098b4881c2d285c6f8b217eccbbb9b
yun8boo <yuma051521@gmail.com>
2021-09-07 22:38:01 +0900

    first commit

---
+++ Today/Reminder.swift
@@ -0,0 +1,15 @@
+//
+//  Reminder.swift
+//  Today
+//
+//  Created by 渡辺悠真 on 2021/09/07.
+//
+
+import Foundation
+
+struct Reminder {
+    var title: String
+    var dueDate: Date
+    var notes: String? = nil
+    var isComplete: Bool = false
+}
